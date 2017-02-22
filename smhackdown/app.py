8b01a5ac28f5ca99e4821635ef0baf3c464e152d
benscott <ben@benscott.co.uk>
2017-02-22 11:07:22 +0000

    API complete

--- smhackdown/app.py
+++ smhackdown/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import os
 from flask_restful import Resource, Api
 from flask_cors import CORS
@@ -7,7 +7,12 @@ import random
 
 db = dataset.connect(os.environ['DATABASE_URL'])
 app = Flask(__name__)
-api = Api(app)
+
+PREFIX = "/api"
+
+api = Api(app, prefix=PREFIX)
+
+cors = CORS(app, resources={r"{0}/*".format(PREFIX): {"origins": "*"}})
 
 class Objects(Resource):
     def get(self):
@@ -25,14 +30,26 @@ class Objects(Resource):
         """
         Save a like to the database
         """
+        object_id = request.json.get('object_id', None)
+        if object_id:
+            likes_tbl = db['likes']
+            likes_tbl.insert(dict(object_id=object_id))
+
         return {}
 
 class TopTen(Resource):
     def get(self):
-        return []       
+        result = db.query('select objects.*, (select count(*) from likes where likes.object_id = id) as count from objects order by count desc limit 10')
+        return list(result)
+
+class TopInstitutions(Resource):
+    def get(self):
+        result = db.query('select institution, count(l.*) as count from objects o left join likes l on l.object_id = o.id group by institution order by count desc')
+        return list(result)
 
 api.add_resource(Objects, '/objects')
-api.add_resource(TopTen, '/topten')
+api.add_resource(TopTen, '/top-ten')
+api.add_resource(TopInstitutions, '/institutions')
 
 if __name__ == '__main__':
     app.run(debug=True)
